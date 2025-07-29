5550a6f0237f193cc3d0acc04ba6526c47fb5343
MuddyThunder1040 <vishnupardha1040@gmail.com>
2025-07-28 22:51:57 -0700

    Add Jenkins pipeline for Terraform deployment

---
+++ Dev-Pipelines/Local-Jenkins.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+
+    environment {
+        REPO_URL = 'https://github.com/MuddyThunder1040/aws-topology.git'
+        REPO_DIR = 'aws-topology'
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git url: "${REPO_URL}", branch: 'main'
+            }
+        }
+        stage('Terraform Init') {
+            steps {
+                dir("${REPO_DIR}") {
+                    sh 'terraform init'
+                }
+            }
+        }
+        stage('Terraform Plan') {
+            steps {
+                dir("${REPO_DIR}") {
+                    sh 'terraform plan'
+                }
+            }
+        }
+        stage('Terraform Apply') {
+            steps {
+                dir("${REPO_DIR}") {
+                    sh 'terraform apply -auto-approve'
+                }
+            }
+        }
+    }
+}
