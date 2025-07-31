52767cdc36917ba9da761823cdbce55cfb49ee1a
MuddyThunder1040 <vishnupardha1040@gmail.com>
2025-07-30 23:06:38 -0700

    Add AWS CLI verification and listing steps to s3.groovy pipeline

--- Test-pipelines/s3.groovy
+++ Test-pipelines/s3.groovy
@@ -5,6 +5,24 @@ pipeline{
         Aws_SECRET_ACCESS_KEY = credentials('aws-secret-key')
     }
     stages {
+        stage('Use AWS CLI') {
+            steps {
+                withCredentials([
+                    usernamePassword(
+                        credentialsId: 'Aws-cli',
+                        usernameVariable: 'AWS_ACCESS_KEY_ID',
+                        passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+                    )
+                ]) {
+                    sh '''
+                        echo "Verifying AWS Identity..."
+                        aws sts get-caller-identity
+
+                        echo "Listing S3 Buckets..."
+                        aws s3 ls
+                    '''
+                }
+            }
         stage('Aws s3 buckets show') {
             steps {
                 echo 'Showing AWS S3 buckets...'
