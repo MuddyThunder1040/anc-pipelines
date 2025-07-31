09686e44c9b77570245d399f6f039674e2198468
MuddyThunder1040 <vishnupardha1040@gmail.com>
2025-07-30 23:03:19 -0700

    Add environment variables for AWS credentials in s3.groovy pipeline

--- Test-pipelines/s3.groovy
+++ Test-pipelines/s3.groovy
@@ -1,5 +1,9 @@
 pipeline{
     agent any 
+    environment {
+        Aws_ACCESS_KEY_ID = credentials('Aws-cli')
+        Aws_SECRET_ACCESS_KEY = credentials('aws-secret-key')
+    }
     stages {
         stage('Aws s3 buckets show') {
             steps {
